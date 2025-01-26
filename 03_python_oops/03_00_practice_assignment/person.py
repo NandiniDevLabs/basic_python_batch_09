0547faaa2a794f3f5083db227de508dad8533b6e
VijayaNandini <vijayanandini0630@gmail.com>
2025-01-26 12:19:09 +0530

    Python OOPs: Constructors and self

---
+++ 03_python_oops/03_00_practice_assignment/person.py
@@ -0,0 +1,19 @@
+# This is an example class
+
+class Person:
+    print("Person class")
+    def __init__(self,name,age,mobile,aadhar):
+        print("This is Person initializer")
+        self.name = name
+        self.age = age
+        self.mobile = mobile
+        self.aadhar = aadhar
+    
+    def update_mobile(self,mobile_num):
+        print("Person class: update_mobile function")
+        self.mobile = mobile_num
+
+    def get_person_attributes(self):
+        print("Person class: get_person_attributes function")
+        return [self.name, self.age, self.mobile, self.aadhar]
+#End of Person Class
